2566253684d4792b76d92d451c9a91499833c57d
jgcaicedo <108642329+jgcaicedo@users.noreply.github.com>
2025-08-01 22:19:30 -0500

    config: mejora configuración de Capacitor

--- todoApp/capacitor.config.ts
+++ todoApp/capacitor.config.ts
@@ -1,9 +1,19 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'todoApp',
-  webDir: 'www'
+  appId: 'com.jgcaicedo.todoapp',
+  appName: 'Todo App',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#3880ff",
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
